c7b427bb62b867c4d489123679f00a205db864db
Zack Allison <zack@monkey.zackallison.com>
2016-08-18 11:53:56 -0400

    Fixed all remaining pylint errors

--- foghornd/Foghorn.py
+++ foghornd/Foghorn.py
@@ -8,7 +8,6 @@ import dateutil.parser
 from twisted.internet import defer
 from twisted.names import dns, error
 
-from FoghornSettings import FoghornSettings
 from GreylistEntry import GreylistEntry
 
 
@@ -21,10 +20,10 @@ class Foghorn(object):
         self.settings = settings
         self._peer_address = None
         self.logging = logging.getLogger('foghornd')
-        self.whitelist = set(self.loadList(self.settings.WhitelistFile))
-        self.blacklist = set(self.loadList(self.settings.BlacklistFile))
+        self.whitelist = set(load_list(self.settings.WhitelistFile))
+        self.blacklist = set(load_list(self.settings.BlacklistFile))
         self.greylist = {}
-        for item in self.loadList(self.settings.GreylistFile):
+        for item in load_list(self.settings.GreylistFile):
             elements = [n.strip() for n in item.split(',')]
             entry = GreylistEntry(
                 elements[0],
@@ -33,11 +32,11 @@ class Foghorn(object):
             )
             self.greylist[elements[0]] = entry
 
-    def saveState(self):
+    def save_state(self):
         """
         Called as the program is shutting down, put shut down tasks here.
         """
-        self.writeList(self.settings.GreylistFile, self.greylist)
+        write_list(self.settings.GreylistFile, self.greylist)
 
     @property
     def peer_address(self):
@@ -50,7 +49,7 @@ class Foghorn(object):
     def peer_address(self, value):
         self._peer_address = value
 
-    def listCheck(self, query):
+    def list_check(self, query):
         """
         Handles the rules regarding what resolves by checking whether the record
         requested is in our lists. Order is important.
@@ -72,7 +71,8 @@ class Foghorn(object):
                     # Is the entry in the greyout period?
                     if curtime - self.settings.BlackOut <= entry.lastSeen:
                         # Is the entry in the blackout period?
-                        self.logging.debug('Allowed by greylist %s ref-by %s', key, self.peer_address)
+                        self.logging.debug('Allowed by greylist %s ref-by %s',
+                                           key, self.peer_address)
                         return True
                     else:
                         self.logging.debug('Rejected/timeout by greylist %s ref-by %s',
@@ -91,57 +91,58 @@ class Foghorn(object):
             entry = GreylistEntry(key)
             self.greylist[key] = entry
             return False
-    def buildResponse(self, query):
+    def build_response(self, query):
         """
         Builds our Sinkholed response to return when disallowing a response.
         """
         name = query.name.name
-        answer = dns.RRHeader(
-            name=name,
-            payload=dns.Record_A(address=b'%s' % (self.settings.Sinkhole)))
+        answer = dns.RRHeader(name=name,
+                              payload=dns.Record_A(address=b'%s' % (self.settings.Sinkhole)))
         answers = [answer]
         authority = []
         additional = []
         return answers, authority, additional
 
-    def query(self, query, timeout=None):
+    def query(self, query):
         """
         Either return our fake response, or let it on through to the next resolver
         in the chain
         """
-        if not self.listCheck(query):
-            return defer.succeed(self.buildResponse(query))
+        if not self.list_check(query):
+            return defer.succeed(self.build_response(query))
         else:
             return defer.fail(error.DomainError())
 
-    def loadList(self, filename):
-        """
-        Load the specified list
-        """
-        lines = []
-        try:
-            with open(filename, mode='r') as read_file:
-                lines = [x.strip() for x in read_file.readlines()]
-                return lines
-        except IOError as e:
-            print "%s" % e
-            return []
 
-    def writeList(self, filename, items):
-        greylistEntries = False
-        if len(items.keys()) > 0 and isinstance(items.itervalues().next(), GreylistEntry):
-            greylistEntries = True
-        else:
-            # We're not going to support writing the other lists at the moment
-            return False
+def write_list(filename, items):
+    """ Write out [gray|whit|black] blists """
+    greylist_entries = False
+    if len(items.keys()) > 0 and isinstance(items.itervalues().next(), GreylistEntry):
+        greylist_entries = True
+    else:
+        # We're not going to support writing the other lists at the moment
+        return False
 
-        try:
-            with open(filename, mode='w') as write_file:
-                if greylistEntries:
-                    for item in items.itervalues():
-                        write_file.write("%s\n" % item)
-                        return True
-        except IOError as e:
-            print "%s" % e
-            return False
+    try:
+        with open(filename, mode='w') as write_file:
+            if greylist_entries:
+                for item in items.itervalues():
+                    write_file.write("%s\n" % item)
+                    return True
+    except IOError as io_error:
+        print "%s" % io_error
+        return False
+
+def load_list(filename):
+    """
+    Load the specified list
+    """
+    lines = []
+    try:
+        with open(filename, mode='r') as read_file:
+            lines = [x.strip() for x in read_file.readlines()]
+            return lines
+    except IOError as io_error:
+        print "%s" % io_error
+        return []
 
