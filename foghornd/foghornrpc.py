d5a43be1d56d5aefba4091fcdb204f0d8bb42281
Zack Allison <zack@monkey.zackallison.com>
2016-09-03 11:48:31 -0400

    API Updates
    Adding routes and functions for API settings

--- foghornd/foghornrpc.py
+++ foghornd/foghornrpc.py
@@ -13,3 +13,33 @@ class FoghornXMLRPC(xmlrpc.XMLRPC):
     def xmlrpc_toggle_baseline(self):
         """Toggle baseline"""
         self.foghorn.toggle_baseline()
+
+    def xmlrpc_reload_lists(self):
+        self.foghorn.reload()
+
+    def xmlrpc_add_to_whitelist(self, host, tag=None):
+        self.foghorn.add_to_whitelist(host, tag)
+
+    def xmlrpc_add_to_blacklist(self, host, tag=None):
+        self.foghorn.add_to_blacklist(host, tag)
+
+    def xmlrpc_add_to_greylist(self, host, tag=None):
+        self.foghorn.add_to_greylist(host, tag)
+
+    def xmlrpc_delete_from_whitelist(self, host):
+        self.foghorn.delete_from_whitelist(host)
+
+    def xmlrpc_delete_from_blacklist(self, host):
+        self.foghorn.delete_from_blacklist(host)
+
+    def xmlrpc_delete_from_greylist(self, host):
+        self.foghorn.delete_from_greylist(host)
+
+    def xmlrpc_delete_tag_from_whitelist(self, tag):
+        self.foghorn.delete_tag_from_whitelist(tag)
+
+    def xmlrpc_delete_tag_from_blacklist(self, tag):
+        self.foghorn.delete_tag_from_blacklist(tag)
+
+    def xmlrpc_delete_tag_from_greylist(self, tag):
+        self.foghorn.delete_tag_from_greylist(tag)
