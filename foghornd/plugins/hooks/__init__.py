556a87c1f850a4864392589e7bf97014626c7d2d
Zack Allison <zack@monkey.zackallison.com>
2016-10-17 14:06:11 -0400

    Added new hooks

--- foghornd/plugins/hooks/__init__.py
+++ foghornd/plugins/hooks/__init__.py
@@ -22,3 +22,47 @@ class HooksBase(object):
     def init(self):
         """Called on startup"""
         pass
+
+    def query(self, peer, query):
+        """Called for every query before processing"""
+        pass
+
+    def failed_acl(self, peer, query):
+        """A query has failed the ACL"""
+        pass
+
+    def no_acl(self, peer, query):
+        """A query type has no ACL"""
+        pass
+
+    def passed(self, peer, query):
+        """A query type has passed greylisting"""
+        pass
+
+    def upstream_error(self, peer, query):
+        """Failure resolving this query"""
+        pass
+
+    def sinkhole(self, peer, query):
+        """A query will be sinkholed"""
+        pass
+
+    def refused(self, peer, query):
+        """A query has been refused"""
+        pass
+
+    def whitelist(self, peer, query):
+        """A query has passed the whitelist"""
+        pass
+
+    def blacklist(self, peer, query):
+        """A query has failed the blacklist"""
+        pass
+
+    def greylist_passed(self, peer, query, msg):
+        """A query has failed the blacklist"""
+        pass
+
+    def greylist_failed(self, peer, query, msg):
+        """A query has failed the blacklist"""
+        pass
